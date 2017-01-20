46f9b107a957238cedf0ad0660d4e9040a505bf8
summit01singh <summit01.singh@gmail.com>
2017-01-20 00:50:49 -0500

    Python Exercise till Jan 19

---
+++ ex1.py
@@ -0,0 +1,7 @@
+print "Hello World"
+#print "Hello Again"
+#print "I like typing this"
+print "Thins is fun"
+print 'Yay! Printing.'
+print "i'd much rather you 'not'."
+print 'I "said do not touch this"'
---
+++ ex4.py
@@ -0,0 +1,17 @@
+my_name = 'sumit kumar singh'
+my_age = 29 # not a lie
+my_height = 5.8 #feet
+my_weights = 145 # lbs
+my_eyes = 'black'
+my_teeth = 'white'
+my_hair = 'black'
+
+
+print "Let's talk about %s." % my_name
+print "He is %f inches tall" % my_height
+print "He's %d pounds heavy." % my_weights
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+#this line is tricky, try to get it exactly right
+print "if I add %d,%f and %d I get %d." % (my_age, my_height, my_weights, my_age + my_height + my_weights)
