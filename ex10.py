380e7e83e3e9b630234f9866f4fff13787a639d6
summit01singh <summit01.singh@gmail.com>
2017-01-20 23:46:11 -0500

    Jan 20 study

---
+++ ex10.py
@@ -0,0 +1,12 @@
+print "How od are you?"
+age = raw_input()
+print "How tall are you?"
+height = raw_input()
+print "How much do you weigh?"
+weight = raw_input()
+print "Do you have girl freind"
+Yes = raw_input()
+
+
+
+print "So, you are %r year old, %r tall and %r heavy and %r I have girl freind" % (age, height, weight, Yes)
